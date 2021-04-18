36cc78e3ac10fffc79b52d57697d7e752604c194
aferodriguez <aferca@hotmail.es>
2021-04-17 20:03:17 -0500

    entidad y modelo agregados, pruebas unitarias respectivas para cada clase

---
+++ src/main/java/com/inditex/pricing/domain/model/Pricing.java
@@ -0,0 +1,67 @@
+/**
+ * company name 2020 - 2021
+ */
+package com.inditex.pricing.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Test Suite for the {@link Pricing} object
+ * 
+ * @author Andres Rodriguez (aferca@hotmail.es)
+ * @version 1.0
+ * @since 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pricing {
+
+	/**
+	 * The pricing Id
+	 */
+	private Integer pricingId;
+
+	/**
+	 * The brand Id
+	 */
+	private Integer brandId;
+
+	/**
+	 * The start date from which the pricing configuration starts to be applied
+	 */
+	private LocalDateTime startDate;
+
+	/**
+	 * the end date until the pricing configuration will be applied
+	 */
+	private LocalDateTime endDate;
+
+	/**
+	 * The product id which is related to the pricing object
+	 */
+	private Integer productId;
+
+	/**
+	 * The priority of this pricing object over other existing pricing register
+	 */
+	private Integer priority;
+
+	/**
+	 * the price for the pricing element
+	 */
+	private BigDecimal price;
+
+	/**
+	 * the currency for this pricing element
+	 */
+	private String currency;
+
+}
---
+++ src/main/java/com/inditex/pricing/persistence/dto/PricingEntity.java
@@ -0,0 +1,87 @@
+/**
+ * company name 2011 - 2021
+ */
+package com.inditex.pricing.persistence.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author Andres Rodriguez (anferca@hotmail.es)
+ * @since 1.0
+ * @version 1.0
+ * 
+ *          Data base representation for Pricing object
+ * 
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "pricing")
+public class PricingEntity implements Serializable {
+
+	/**
+	 * The pricing Id
+	 */
+	@Id
+	@Column(name = "pricingId")
+	private Integer pricingId;
+
+	/**
+	 * The brand Id
+	 */
+	@Column(name = "brand_id")
+	private Integer brandId;
+
+	/**
+	 * The start date from which the pricing configuration starts to be applied
+	 */
+	@Column(name = "start_date")
+	private LocalDateTime startDate;
+
+	/**
+	 * the end date until the pricing configuration will be applied
+	 */
+	@Column(name = "end_date")
+	private LocalDateTime endDate;
+
+	/**
+	 * The product id which is related to the pricing object
+	 */
+	@Column(name = "product_id")
+	private Integer productId;
+
+	/**
+	 * The priority of this pricing object over other existing pricing register
+	 */
+	@Column(name = "priority")
+	private Integer priority;
+
+	/**
+	 * the price for the pricing element
+	 */
+	@Column(name = "price")
+	private BigDecimal price;
+
+	/**
+	 * the currency for this pricing element
+	 */
+	@Column(name = "currency")
+	private String currency;
+
+}
